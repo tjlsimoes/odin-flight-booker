0da9eefd4816b5661e19f63b1cab5dd7307f443c
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-25 14:14:52 +0100

    Add Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_095150) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_131427) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "billing_address"
+    t.integer "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -28,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_095150) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
