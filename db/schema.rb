f1d558786050e3b7f3c4a0e77fcdc94f520017b3
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-25 16:09:13 +0100

    Add bookings_passengers join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_135807) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_150705) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -26,9 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_135807) do
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
-  create_table "bookings_flights", id: false, force: :cascade do |t|
+  create_table "bookings_passengers", id: false, force: :cascade do |t|
     t.integer "booking_id", null: false
-    t.integer "flight_id", null: false
+    t.integer "passenger_id", null: false
   end
 
   create_table "flights", force: :cascade do |t|
