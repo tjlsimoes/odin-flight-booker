c9a742070b1d976772570fdaaaa27f7b68ba3976
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-25 14:20:34 +0100

    Add Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_131427) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_132017) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_131427) do
     t.string "start"
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bookings", "flights"
