6fde91aef1e762cbb7200405f06f222d26c559c3
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-25 15:10:37 +0100

    Add bookings#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new(flight_id: selection_params[:selected_flight_id],
+                nbr_passengers: selection_params[:nbr_passengers])
+    @passengers = []
+    
+    selection_params[:nbr_passengers].times do
+      @passengers << Passenger.new
+    end
+  end
+
+  private
+  def selection_params
+    params.require(:booking).permit(:nbr_passengers,
+                              :selected_flight_id)
+  end
 end
