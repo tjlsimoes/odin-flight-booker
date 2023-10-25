aa828593aeddf4b5a7843cf9656b9066b281490a
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-25 14:27:39 +0100

    Add association between Flight and Booking
    Flight has many bookings. Booking belongs to Flight.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
 
 	def self.restrain(search)
 		if search
