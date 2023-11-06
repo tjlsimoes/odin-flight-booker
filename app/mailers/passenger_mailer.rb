2c4c1153ee52efc5ba92b261bef4dccafc592b28
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-06 19:08:47 +0000

    Flesh out passenger_mailer#confirmation_email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,7 @@
 class PassengerMailer < ApplicationMailer
+  def confirmation_email
+    @name = params[:name]
+    @email = params[:email]
+    mail(to: @email, subject: 'You have booked your ticket')
+  end
 end
