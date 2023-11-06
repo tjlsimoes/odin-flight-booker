337673241b87f7b28a64255aa54eafc1a4b68d07
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-06 16:16:16 +0000

    Standardize new passenger fields id attributes
    Format them in accordance to the originals.
    Function add() could use a refactoring.

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -17,23 +17,26 @@ export default class extends Controller {
     const clone = template.content.cloneNode(true)
 		pass_fields.appendChild(clone)
 
-    const name_attr = `booking[passengers_attributes][${nbr_passengers - 1}][name]`;
-    const email_attr = `booking[passengers_attributes][${nbr_passengers - 1}][email]`;
+    const pname_name_attr = `booking[passengers_attributes][${nbr_passengers - 1}][name]`;
+    const pemail_name_attr = `booking[passengers_attributes][${nbr_passengers - 1}][email]`;
+  
+    const pname_id_attr = `booking_passengers_attributes_${nbr_passengers - 1}_name`;
+    const pemail_id_attr = `booking_passengers_attributes_${nbr_passengers - 1}_email`;
 
     // Strings are different for filling out id value.
 
-    this.nameTargets.slice(-1)[0].setAttribute("name", name_attr)
-    this.emailTargets.slice(-1)[0].setAttribute("name", email_attr)
+    this.nameTargets.slice(-1)[0].setAttribute("name", pname_name_attr)
+    this.emailTargets.slice(-1)[0].setAttribute("name", pemail_name_attr)
 
-    this.nameTargets.slice(-1)[0].setAttribute("id", name_attr)
-    this.emailTargets.slice(-1)[0].setAttribute("id", email_attr)
+    this.nameTargets.slice(-1)[0].setAttribute("id", pname_id_attr)
+    this.emailTargets.slice(-1)[0].setAttribute("id", pemail_id_attr)
 
     let i = 1;
     this.labelTargets.slice(-2).map((label) => {
       if (i == 1) {
-        label.setAttribute("for", name_attr)
+        label.setAttribute("for", pname_id_attr)
       } else {
-        label.setAttribute("for", email_attr)
+        label.setAttribute("for", pemail_id_attr)
       }
       i++;
     })
