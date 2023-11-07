44ebba1eed1ebce068d048d4421002097ce46cf0
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-07 15:13:31 +0000

    Recode #confirmation_email to take optional passenger parameter

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,7 +1,12 @@
 class PassengerMailer < ApplicationMailer
-  def confirmation_email
-    @name = params[:name]
-    @email = params[:email]
+  def confirmation_email(*passenger)
+    if passenger.empty?
+      @name = params[:name]
+      @email = params[:email]
+    else
+      @name = passenger[0].name
+      @email = passenger[0].email
+    end
     mail(to: @email, subject: 'You have booked your ticket')
   end
 end
